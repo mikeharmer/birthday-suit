b7ff0d950dd2441fe1938e3a60e33d25c150372b
Mike H <mikeh@neverbland.com>
2013-08-14 14:40:04 +0100

    New default jQuery version

--- _footer.php
+++ _footer.php
@@ -6,7 +6,7 @@
 
 
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 
 <script src="_assets/js/app.js"></script>
 
